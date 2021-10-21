6fab6ac4ab56f029742a2f4658b547f6e94a7864
Minhaz19 <minhazulabedin44@gmail.com>
2021-10-21 17:39:05 +0600

    hotel and car booking done

---
+++ model/hotelBookingSchema.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const hotelBookingSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  hotelName: {
+    type: String,
+    required: true,
+  },
+  roomType: {
+    type: String,
+    required: true,
+  },
+  checkIn: {
+    type: Date,
+    required: true,
+  },
+  checkOut: {
+    type: Date,
+    required: true,
+  },
+  roomType: {
+    type: String,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  paymentId: {
+    type: String,
+    required: true,
+  },
+  paymentTime: {
+      type: Date,
+      required: true
+  },
+  adult: {
+    type: String,
+    required: true,
+  }
+});
+
+const hotelBooking = mongoose.model("hotelBooking", hotelBookingSchema);
+
+module.exports = hotelBooking;
